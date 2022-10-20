85ecb1fff0b7acb85a263adac1cefe67b1524137
ekeneezeani <108833282+ekeneezeani@users.noreply.github.com>
2022-10-20 02:15:42 -0500

    added product count and categories to the dashboard

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,4 +1,17 @@
 class Admin::DashboardController < ApplicationController
   def show
+    @count = Product.all
+
+    @all_products = Product.all
+    
+    @count = @all_products.size
+ 
+    @categories = @all_products.select(:category_id).distinct
+    @category_list =[];
+    @categories.each do |e|
+      @category_list.push(Category.find(e.category_id).name)
+    end
+    
+    puts "CATEGORIES  #{@category_list}"
   end
 end
